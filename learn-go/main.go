e9d7b41fbca4d9102c5ef7be5e67a60a4ca63121
James Thomas <mrcloudmustache@gmail.com>
2025-01-23 07:05:20 -0600

    Multiple return values

--- learn-go/main.go
+++ learn-go/main.go
@@ -2,14 +2,10 @@ package main
 
 import "fmt"
 
-func newCard() string {
-	return "King of hearts"
-}
-
 func main() {
-	cards := deck{"Ace", "Jack", "Ten", newCard()}
-	cards = append(cards, "Queen")
-	cards.print()
-
-	fmt.Println(newDeck())
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	fmt.Println("------")
+	remainingCards.print()
 }
